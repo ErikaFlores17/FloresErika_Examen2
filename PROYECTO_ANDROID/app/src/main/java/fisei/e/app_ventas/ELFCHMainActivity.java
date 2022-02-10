04980984d8b9e6be498c4562dcc9e31297ccef4e
ErikaFlores17 <erikaflores046@gmail.com>
2022-02-09 19:24:40 -0500

    Examen 19:20

--- PROYECTO_ANDROID/app/src/main/java/fisei/e/app_ventas/ELFCHMainActivity.java
+++ PROYECTO_ANDROID/app/src/main/java/fisei/e/app_ventas/ELFCHMainActivity.java
@@ -2,7 +2,6 @@ package fisei.e.app_ventas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.StrictMode;
@@ -10,8 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.google.android.material.button.MaterialButtonToggleGroup;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -32,7 +29,6 @@ public class MainActivity extends AppCompatActivity  {
         editTextPassword=(EditText) findViewById(R.id.editTextPassword);
 
         buttonIngresar1=(Button) findViewById(R.id.buttonIngresar);
-
         buttonIngresar1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
